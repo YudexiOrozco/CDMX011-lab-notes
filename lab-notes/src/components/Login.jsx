9d579b56e55f46507d3cb14480a0281afec2c269
YudexiOrozco <86724658+YudexiOrozco@users.noreply.github.com>
2021-10-20 01:33:31 -0500

    styles with login and notes

--- lab-notes/src/components/Login.jsx
+++ lab-notes/src/components/Login.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import logo from '../images/logo.png'
-import cook from '../images/cooking.jpg'
+import cook from '../images/cooking.png'
+import google from '../images/google.png'
+import vector from '../images/vector.png'
 import { auth, db } from '../firebase'
 import {withRouter} from 'react-router-dom'
 
 const Login = (props) => {
 
-  const [email, setEmail] = React.useState('')
-  const [password, setPassword] = React.useState('')
-  const [error, setError] = React.useState(null)
-  const [registro, setRegistro] = React.useState(false)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState(null)
+  const [registro, setRegistro] = useState(false)
 
   const procesarDatos = (e) =>{
+
     e.preventDefault()
     if(!email.trim()){
       // console.log('Ingrese Email')
@@ -26,6 +29,7 @@ const Login = (props) => {
     if(password.length < 6){
       // console.log('Password mayor a 6 caracteres')
       setError('Password mayor a 6 caracteres')
+      return
     }
     setError(null)
     console.log('Pasando todas las validaciones!')
@@ -82,16 +86,33 @@ const Login = (props) => {
 
   }, [email, password, props.history])
 
+
+  const focus = (e) => {
+    e.currentTarget.previousElementSibling.classList.add('top')
+    e.currentTarget.previousElementSibling.classList.add('focus');
+    e.currentTarget.parentNode.classList.add('focus');
+  }
+
+  const blur = (e) => {
+    e.currentTarget.value = e.currentTarget.value.trim();
+    if(e.currentTarget.value.trim().length === 0 ){
+       e.currentTarget.previousElementSibling.classList.remove('top');
+    }
+   
+    e.currentTarget.previousElementSibling.classList.remove('focus');
+    e.currentTarget.parentNode.classList.remove('focus');
+  }
+
   return (
     <div className='container-login'>
-      <img className='logo' src={logo} alt='logo app foodpad'/>
+      <img className='logo-login' src={logo} alt='logo app foodpad'/>
       <img className='cook' src={cook} alt='cooking'/>
-      <h3 className='slogan'>Manage, organize and save your favorite recipes</h3>
-      <h4 className='text-register'>
+      <h3 className='slogan'>Manage, organize and save your favorite recipes.</h3>
+      {/* <h4 className='text-register'>
         {
-          registro ? 'Registro de usuarios' : 'Login de acceso'
+          registro ? 'Create an account' : 'Login registered user'
         }
-      </h4>
+      </h4> */}
       <div className='container'>
         <div className='container-form'>
           <form onSubmit={procesarDatos}>
@@ -100,37 +121,50 @@ const Login = (props) => {
                 <div>{error}</div>
               )
             }
-            <input 
-              type = 'email' 
-              className = 'inputs'
-              placeholder = 'Ingrese un email'
-              onChange = {e => setEmail(e.target.value)}
-              value = {email}
-            />
+            <div className='div-input'> 
+              <label className='inputs'>
+                <span className='span-email'>Email</span>
+                <input 
+                  type = 'email' 
+                  className = 'input-email'
+                  onChange = {e => setEmail(e.target.value)}
+                  onFocus={ (e) => { focus(e)} }
+                  onBlur={ (e) => { blur(e)} }
+                  value = {email}
+                />
+              </label>
+           
+            <label className='inputs'>
+            <span className='span-email'>Password</span>
             <input 
               type = 'password' 
-              className = 'inputs'
-              placeholder = 'Ingrese una contraseña'
+              className = 'input-email'
               onChange = {e => setPassword(e.target.value)}
+              onFocus={ (e) => { focus(e)} }
+              onBlur={ (e) => { blur(e)} }
               value = {password}
             />
+             </label> 
+            </div>
             <button className='btn-login' type='submit'>
               {
-                registro ? 'Registrarse' : 'Acceder'
+                registro ? 'Sign Up' : 'Log In'
               }
             </button>
+            <button className='btn-google' ><img  src={google} alt='btn-google'/>With Google</button>
             <button 
-              className='btn-login' 
+              className='btn-new' 
               onClick={() => setRegistro(!registro)}
               type='button'
             >
              {
-               registro ? '¿Ya estas registrado?' : '¿No tienes cuenta?'
+               registro ? 'You already sign up?' : 'New account'
              }
             </button>
           </form>
         </div>
       </div>
+      <img className='vector' src={vector} alt='vector'/>
     </div>
   )
 }
--- lab-notes/src/components/Wall.jsx
+++ lab-notes/src/components/Wall.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import {auth, db} from '../firebase'
-import {withRouter} from 'react-router-dom'
+import React, { useState } from 'react'
+import { auth, db } from '../firebase'
+import { withRouter } from 'react-router-dom'
+import icon from '../images/icon-wall.png'
+import logo from '../images/logo.png'
+import close from '../images/close.png'
+import styled from 'styled-components'
 
 const Wall = (props) => {
 
-  const [user, setUser] = React.useState(null)
-  const [tareas, setTareas] = React.useState([])
-  const [tarea, setTarea] = React.useState('')
-  const [modoEdicion, setModoEdicion] = React.useState(false)
-  const [id, setId] = React.useState('')
+  const [user, setUser] = useState(null)
+  const [tareas, setTareas] = useState([])
+  const [tarea, setTarea] = useState('')
+  const [content, setContent] = useState('')
+  const [modoEdicion, setModoEdicion] = useState(false)
+  const [id, setId] = useState('')
+  const [modal, setModal] = useState(false);
 
   const cerrarSesion = () => {
       auth.signOut()
@@ -20,7 +26,7 @@ const Wall = (props) => {
   React.useEffect(() => {
     const getData = async () => {
       try {
-        const data = await db.collection('tareas').get()
+        const data = await db.collection('tareas').doc(auth.currentUser.email).collection('recipes').orderBy("fecha", "desc").get()
         const arrayData = data.docs.map(doc => ({ id: doc.id, ...doc.data() }))
         // console.log(arrayData)
         setTareas(arrayData)
@@ -48,19 +54,26 @@ const Wall = (props) => {
       console.log('Esta vacio')
       return
     }
+    if(!content.trim()){
+      console.log('Esta vacio')
+      return
+    }
     try {
       const nuevaTarea = {
         name: tarea,
-        fecha: Date.now(),
+        description: content,
+        fecha: Date.now(), // new Date("October 5, 2021 13:30:00")
       }
-      const data = await db.collection('tareas').add(nuevaTarea)
+
+      const data = await db.collection('tareas').doc(user.email).collection('recipes').add(nuevaTarea)
 
       setTareas([
-        ...tareas,
-        {...nuevaTarea, id: data.id}
+        {...nuevaTarea, id: data.id}, ...tareas,
       ])
 
       setTarea('')
+      setContent('')
+      setModal(false)
     } catch (error) {
       console.log(error)
     }
@@ -69,7 +82,7 @@ const Wall = (props) => {
 
   const eliminar = async (id) => {
     try {
-      await db.collection('tareas').doc(id).delete()
+      await db.collection('tareas').doc(user.email).collection('recipes').doc(id).delete()
 
       const arrayFiltrado = tareas.filter(item => item.id !== id)
       setTareas(arrayFiltrado)
@@ -82,6 +95,7 @@ const Wall = (props) => {
   const activarEdicion = (item) => {
     setModoEdicion(true)
     setTarea(item.name)
+    setContent(item.description)
     setId(item.id)
   }
 
@@ -91,78 +105,153 @@ const Wall = (props) => {
       console.log('vacio')
       return
     }
+    if(!content.trim()){
+      console.log('vacio')
+      return
+    }
     try {
-      await db.collection('tareas').doc(id).update({
-         name: tarea
+      await db.collection('tareas').doc(user.email).collection('recipes').doc(id).update({
+         name: tarea,
+         description: content,
       })
       const arrayEditado = tareas.map(item => (
-        item.id === id ? {id: item.id, fecha: item.fecha, name: tarea} : item
+        item.id === id ? {id: item.id, fecha: item.fecha, name: tarea, description: content} : item
       ))
       setTareas(arrayEditado)
       setModoEdicion(false)
       setTarea('')
+      setContent('')
       setId('')
+      setModal(false)
     } catch (error) {
       console.log(error)
     }
   }
 
-
   return (
-    <div>
-      <h2>Lista de Recetas</h2>
-      <button onClick={() => cerrarSesion()}>Cerrar sesión</button>
-      {
-        user && (
-          <h3>{user.email}</h3>
-        )
-      }
+    <div className='first-div'>
+     <div className='encabezado'>
+       <img className='logo' src={logo} alt='logo-foodpad'/> 
+        {
+          user && (
+            <h3 className='welcome'>Welcome {user.email}</h3>
+          )
+        }
+        <button className='btn-logout' onClick={() => cerrarSesion()}>Sign out</button>
+      </div>
       <div className='container-tareas'>
-        <div>
-          <ul>
             {
               tareas.map(item => (
-                <li key={item.id}>
-                  {item.name}
-                  <button onClick={() => eliminar(item.id)}>
+                <div className='div-recipes' key={item.id}>
+                  <b>{item.name}</b> <hr/>
+                   <div
+                    onClick={() => {activarEdicion(item); setModal(!modal)}}
+                  
+                  >{item.description}</div>
+                  <div className='btn-tareas'>
+                  <button className = 'btns' onClick={() => eliminar(item.id)}>
                     Delete
                   </button>
                   <button
-                   onClick={() => activarEdicion(item)}
+                  className = 'btns'
+                  onClick={() => {activarEdicion(item); setModal(!modal)}}
                   >
                     Edit
                   </button>
-                </li>
+                  <h3>{new Date(item.fecha).toDateString() }</h3>
+                  </div>
+                </div>
               ))
             }
-          </ul>
-        </div>
-        <div className='formulario'>
-          <h3>
-            {
-              modoEdicion ? 'Editar Tarea' : 'Agregar Tarea'
-            }
-          </h3>
-          <form onSubmit={modoEdicion ? editar : agregar}>
-          <input
-            type='text'
-            placeholder='Ingrese Receta'
-            className='form-input'
-            onChange={e => setTarea(e.target.value)}
-            value={tarea}
-          />
-          <button 
-            type='submit'
-          >
-            {
-              modoEdicion ? 'Editar' : 'Agregar'
-            }
-          </button>
-          </form>
-        </div>
+        
+      </div>
+        <button 
+          className='icon-plus'
+          onClick={() => setModal(!modal)}
+          ><img src={icon} alt='plus'/></button>
+      
+      <div className='formulario'>
+              {modal && 
+              <ContenedorModal>
+                <Contenido>
+                <h3 className='title-modal'>
+                {
+                  modoEdicion ? 'Edit recipe' : 'Add recipe'
+                }
+                </h3>
+                  <form onSubmit={modoEdicion ? editar : agregar}>
+                    <input
+                      type='text'
+                      placeholder = 'Add a title'
+                      className = 'input-title'
+                      onChange = {e => setTarea(e.target.value)}
+                      value = {tarea}
+                    />
+                    <textarea
+                      className = 'input-pass'
+                      type = 'text'
+                      placeholder='Content...'
+                      onChange = {e => setContent(e.target.value)}
+                      value = {content}
+                    ></textarea>
+                    <button 
+                      className='btn-submit'
+                      type = 'submit'
+                    >
+                      {
+                        modoEdicion ? 'Edit' : 'Add'
+                      }
+                    </button>
+                  </form>
+                </Contenido>
+                <BotonCerrar 
+                  className='btn-close'
+                  onClick={() => setModal(false)}
+                  ><img src={close} alt='close'/></BotonCerrar>
+              </ContenedorModal>
+              }
       </div>
     </div>
   )
 }
 
 export default withRouter(Wall)
+
+
+
+
+const ContenedorModal = styled.div`
+  width: 300px;
+  min-height: 200px;
+  background: #fff;
+  position: relative;
+  border-radius: 5px;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+  
+  padding: 20px;
+  left: 90px;
+`;
+
+const BotonCerrar = styled.button`
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  
+  width: 30px;
+  height: 30px;
+  border: none;
+  background: none;
+  cursor: pointer;
+  transition: .3s ease all;
+  border-radius: 5px;
+  font-weight: bold;
+  font-size: 25px;
+
+  &:hover {
+    background: #f2f2f2;
+  }
+`;
+
+const Contenido = styled.div`
+ 
+`;
